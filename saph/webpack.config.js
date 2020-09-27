beff691308788a8f0eb240fb8601f351982eec93
yejin <dbswjd1977@gmail.com>
2020-09-27 19:27:14 +0900

    js,css 연결

--- saph/webpack.config.js
+++ saph/webpack.config.js
@@ -1,4 +1,50 @@
 module.exports = {
     mode : 'development',
     entry : './static/js/index.js',
+    output: {
+      publicPath: 'http://localhost:8080/'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
+          }
+        },
+        {
+          test: /\.vue$/,
+          loader: 'vue-loader',
+          options: {
+            loaders: {}
+          }
+        },
+        {
+          test: /\.(png|jpg|gif|svg)$/,
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]?[hash]'
+          }
+        },
+      ],
+    },
+    resolve:{  
+      alias :{
+        'vue$': 'vue/dist/vue.esm.js'
+      },
+      extensions: ['*', '.js', '.vue', '.json']
+    },
+    devServer: {  
+      headers: {
+          'Access-Control-Allow-Origin': '*', 
+      }
+    }
   }
